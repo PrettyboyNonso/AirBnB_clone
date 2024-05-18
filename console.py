ba5e81d3895fad1f34f37040c79ee4959729feb1
a3ela <abelsintayehu626@gmail.com>
2024-05-19 01:17:39 +0300

    done first phase

---
+++ console.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Console Module"""
+import cmd
+import sys
+
+
+class HBNBCommand(cmd.Cmd):
+    """Contains the functionality for the console"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, command):
+        """Method to exit the HBNB console"""
+        exit()
+
+    def help_quit(self):
+        """Print the help documentation for the quit command"""
+        print("Exits the program with formatting\n")
+
+    def do_EOF(self, arg):
+        """Handles EOF to exit the console"""
+        print()
+        exit()
+
+    def help_EOF(self):
+        """Prints the help documentation for the EOF"""
+        print("Exit the program without formatting\n")
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
