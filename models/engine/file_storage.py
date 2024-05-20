102d80ce1222e1f8d5aa2b40c7f7dd7da188c907
PrettyboyNonso <135073822+PrettyboyNonso@users.noreply.github.com>
2024-05-20 09:17:22 +0100

    Update file_storage.py6

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
+import os.path
 import json
 from models.base_model import BaseModel
-from models.user import User
 
 class FileStorage:
     """Serializes instances to a JSON file & deserializes back to instances"""
@@ -24,13 +24,13 @@ class FileStorage:
             json.dump(obj_dict, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects (only if the JSON file (__file_path) exists)"""
-        try:
+        """Deserializes the JSON file to __objects"""
+        if os.path.exists(self.__file_path):
             with open(self.__file_path, 'r') as f:
                 obj_dict = json.load(f)
             for key, value in obj_dict.items():
-                class_name = value["__class__"]
-                cls = globals()[class_name]
-                self.__objects[key] = cls(**value)
-        except FileNotFoundError:
-            pass
+                class_name = value.get("__class__")
+                if class_name:
+                    cls = globals().get(class_name)
+                    if cls:
+                        self.__objects[key] = cls(**value)
